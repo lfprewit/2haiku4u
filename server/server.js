24096ef181cda3bdf8a8259dffff118de9a0c2f7
Lance <lfprewit@gmail.com>
2020-07-20 22:41:38 -0400

    fixed port issue with heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 const express = require ('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
