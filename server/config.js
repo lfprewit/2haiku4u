875840d6f27ecc49ed4f23c185a270525c91cb11
Lance <lfprewit@gmail.com>
2020-07-22 22:02:15 -0400

    merge this to master

--- server/config.js
+++ server/config.js
@@ -5,38 +5,3 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost'
 };
 
-
-
-
-
-
-var apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-//var apiUrl = 'http://localhost:5000';
-
-export default apiUrl;
-
-
-require('dotenv').config(); 
-module.exports = {
-"development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-},
-"test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-},
-"production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-}
-};
