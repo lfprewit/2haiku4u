707459d96c6dc1f2046e7552b7e8fa12c8543868
Lance <lfprewit@gmail.com>
2020-07-25 11:52:46 -0400

    trivial change

--- server/config.js
+++ server/config.js
@@ -2,7 +2,8 @@ module.exports = {
     database: process.env.DB_DATABASE || 'Haiku',
     username: process.env.DB_USERNAME || 'username',
     password: process.env.DB_PASSWORD || '9999ball',
-    host: process.env.DATABASE_URL || 'localhost'
+    host: process.env.DATABASE_URL || 'localhost'   
+    
 };
 
 // module.exports = {
